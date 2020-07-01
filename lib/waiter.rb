87ca6ac521f2687e6d27347a20e64c768e86c8d3
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-01 18:20:42 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
-
+ 
+  attr_accessor :name, :yrs_experience
+  
+  @@allwaiters = [] 
+  
+  def initialize
+    @name = name
+    @yrs_experience = yrs_experience
+    @@allwaiters << self
+  end 
+  
+  def self.all 
+    @@allwaiters
+  end 
 end
